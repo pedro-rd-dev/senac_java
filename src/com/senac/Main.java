9e6fe858a2fbddfb93803241a44a121fb855d19d
pedro-rd-dev <pedro.rd.dev@gmail.com>
2021-03-16 19:19:04 -0300

    Adicionando comentario da main

--- src/com/senac/Main.java
+++ src/com/senac/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Usuario usuario = new Usuario();
-        usuario.verficarIdade(20);
+        usuario.cadastrar(18,true,true);
 
     }
 }
--- src/com/senac/models/Usuario.java
+++ src/com/senac/models/Usuario.java
@@ -2,12 +2,48 @@ package com.senac.models;
 
 public class Usuario {
 
-    public void verficarIdade(int idade){
+    public boolean verficarIdade(int idade){
 
         if(idade >=18){
             System.out.println("É maior de idade");
+            return true;
         }else {
             System.out.println("Não é maior de idade");
+            return false;
         }
+    }
+
+    public boolean verificarNacionalidade(boolean nacionalidade) {
+        if(nacionalidade){
+            System.out.println("É BRASILEIRO");
+            return true;
+        }else {
+            System.out.println("NÃO É BRASILEIRO");
+            return false;
+        }
+    }
+
+    public boolean verificarDisponibilidade(boolean disponibilidade) {
+        if(disponibilidade){
+            System.out.println("tem disponibilidada");
+            return true;
+        }else {
+            System.out.println("Não tem disponibilidada");
+            return false;
+        }
+    }
+
+    public boolean cadastrar(int idade,boolean nacionalidade,boolean disponibilidade) {
+        if(verficarIdade(idade)){
+            if(verificarNacionalidade(nacionalidade)){
+                if(verificarDisponibilidade(disponibilidade)){
+                    System.out.println("PARABENS VOCE CADASTROU");
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
